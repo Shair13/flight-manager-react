aa13a2553e028fde28b186d53796d3d96e26cc89
Szer13 <shad350@gmail.com>
2024-06-17 17:30:40 +0200

    added button to passenger section

--- src/components/Passenger.js
+++ src/components/Passenger.js
@@ -1,4 +1,6 @@
-import {TableCell, TableRow} from "@mui/material";
+import {Button, Stack, TableCell, TableRow} from "@mui/material";
+import {Link} from "react-router-dom";
+import React from "react";
 
 const Passenger = ({passengerId, name, surname, phone}) => {
 
@@ -8,6 +10,16 @@ const Passenger = ({passengerId, name, surname, phone}) => {
             <TableCell align={"center"}>{name}</TableCell>
             <TableCell align={"center"}>{surname}</TableCell>
             <TableCell align={"center"}>{phone}</TableCell>
+            <TableCell align={"center"}>
+                <Stack spacing={1} direction="row" justifyContent="center">
+                    <Link to={`/flights/${passengerId}`}>
+                        <Button variant="contained">Update</Button>
+                    </Link>
+                    <Link to={`/flights/${passengerId}`}>
+                        <Button variant="outlined" color="error">Delete</Button>
+                    </Link>
+                </Stack>
+            </TableCell>
         </TableRow>
     );
 }
