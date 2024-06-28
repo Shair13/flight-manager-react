8175c06254304bcd768d079cc41c4b3cc0d46a92
Szer13 <shad350@gmail.com>
2024-06-28 16:46:27 +0200

    removed non-used import

--- src/components/DeleteFlightButton.js
+++ src/components/DeleteFlightButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button} from "@mui/material";
 import {deleteFlight, getFlights} from "../logic/flightsApi";
-import {redirect, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const DeleteFlightButton = ({flightId, setFlights}) => {
 
